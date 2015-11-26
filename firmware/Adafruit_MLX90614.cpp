553da781dfaaac9aaa619be75a859e656b61356b
Matt White <mwhite@immersiveps.com>
2015-11-25 22:44:24 -0500

    modified to support multiple devices.

--- firmware/Adafruit_MLX90614.cpp
+++ firmware/Adafruit_MLX90614.cpp
@@ -1,4 +1,4 @@
-/*************************************************** 
+/***************************************************
   This is a library for the MLX90614 Temp Sensor
 
   Designed specifically to work with the MLX90614 sensors in the
@@ -6,13 +6,13 @@
   ----> https://www.adafruit.com/products/1748
   ----> https://www.adafruit.com/products/1749
 
-  These sensors use I2C to communicate, 2 pins are required to  
+  These sensors use I2C to communicate, 2 pins are required to
   interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Written by Limor Fried/Ladyada for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
@@ -23,9 +23,11 @@ Adafruit_MLX90614::Adafruit_MLX90614(uint8_t i2caddr) {
 }
 
 
-boolean Adafruit_MLX90614::begin(void) {
+boolean Adafruit_MLX90614::begin(uint8_t i2caddr) {
+  _addr = i2caddr;
+  //Serial.println("Setting Address");
   Wire.begin();
-
+  //Serial.println("Wire running");
   /*
   for (uint8_t i=0; i<0x20; i++) {
     Serial.print(i); Serial.print(" = ");
@@ -53,15 +55,20 @@ double Adafruit_MLX90614::readObjectTempC(void) {
 
 
 double Adafruit_MLX90614::readAmbientTempC(void) {
+  //Serial.println("Read Ambient Temp C");
   return readTemp(MLX90614_TA);
 }
 
 float Adafruit_MLX90614::readTemp(uint8_t reg) {
+  //Serial.println("Read Temp Function");
+
   float temp;
-  
+
   temp = read16(reg);
   temp *= .02;
   temp  -= 273.15;
+
+  //Serial.println(temp);
   return temp;
 }
 
@@ -70,10 +77,10 @@ float Adafruit_MLX90614::readTemp(uint8_t reg) {
 uint16_t Adafruit_MLX90614::read16(uint8_t a) {
   uint16_t ret;
 
-  Wire.beginTransmission(_addr); // start transmission to device 
+  Wire.beginTransmission(_addr); // start transmission to device
   Wire.write(a); // sends register address to read from
   Wire.endTransmission(false); // end transmission
-  
+
   Wire.requestFrom(_addr, (uint8_t)3);// send data n-bytes read
   ret = Wire.read(); // receive DATA
   ret |= Wire.read() << 8; // receive DATA
